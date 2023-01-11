e8c8317c5312e0c420bbed9fa5877fc202e413a1
Jules Lasne <jules.lasne@gmail.com>
2023-01-11 10:23:24 +0100

    Chore: Version bump

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def get_file_text(file_name):
 
 setup(
     name="edusign",
-    version="0.0.7",
+    version="0.0.8",
     author="Jules Lasne",
     author_email="jules.lasne@gmail.com",
     url="https://github.com/seluj78/edusign",
